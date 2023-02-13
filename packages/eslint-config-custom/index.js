5f3613918401517db112c027b6269bfce103081d
Franco RATOVOSON <jean.franco.ratovoson@gmail.com>
2023-02-13 18:38:02 +0300

    setup

--- packages/eslint-config-custom/index.js
+++ packages/eslint-config-custom/index.js
@@ -1,7 +1,13 @@
 module.exports = {
-  extends: ["next", "turbo", "prettier"],
+  extends: ["airbnb-base", "turbo", "prettier"],
+  parser: "@typescript-eslint/parser",
+  plugins: [
+        "@typescript-eslint",
+        "prettier"
+    ],
   rules: {
     "@next/next/no-html-link-for-pages": "off",
     "react/jsx-key": "off",
+    "@typescript-eslint/no-unused-vars": ["warn",{ "varsIgnorePattern": "^_", "argsIgnorePattern": "^_" }]
   },
 };
